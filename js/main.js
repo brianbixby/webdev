101cc0ca0d01804744b77c92dc1d62d06812d97e
brianbixby0@gmail.com <brianbixby0@gmail.com>
2017-03-06 23:58:03 -0800

    working on modals

--- js/main.js
+++ js/main.js
@@ -1,15 +1,72 @@
-$(document).ready(function() {
-  function setHeight() {
-    windowHeight = $(window).innerHeight();
-    windowWidth = $(window).innerWidth();
-    $('#head').css('min-height', (.6*windowHeight));
-    // if(windowWidth > 767) {
-    //   $('#').css('min-height', windowHeight);
-    // }
-  };
-  setHeight();
+// $(document).ready(function() {
+//   function setHeight() {
+//     windowHeight = $(window).innerHeight();
+//     windowWidth = $(window).innerWidth();
+//     $('#head').css('min-height', (.6*windowHeight));
+//     // if(windowWidth > 767) {
+//     //   $('#').css('min-height', windowHeight);
+//     // }
+//   };
+//   setHeight();
+//
+//   $(window).resize(function() {
+//     setHeight();
+//   });
+// });
+// $('a').click(function(){
+//     $('html, body').animate({
+//         scrollTop: $( $(this).attr('href') ).offset().top
+//     }, 500);
+//     return false;
+// });
 
-  $(window).resize(function() {
-    setHeight();
-  });
+// var sections = $('section')
+//   , nav = $('nav')
+//   , nav_height = nav.outerHeight();
+//
+// $(window).on('scroll', function () {
+//   var cur_pos = $(this).scrollTop();
+//
+//   sections.each(function() {
+//     var top = $(this).offset().top - nav_height,
+//         bottom = top + $(this).outerHeight();
+//
+//     if (cur_pos >= top && cur_pos <= bottom) {
+//       nav.find('a').removeClass('active');
+//       sections.removeClass('active');
+//
+//       $(this).addClass('active');
+//       nav.find('a[href="#'+$(this).attr('id')+'"]').addClass('active');
+//     }
+//   });
+// });
+//
+// nav.find('a').on('click', function () {
+//   var $el = $(this)
+//     , id = $el.attr('href');
+//
+//   $('html, body').animate({
+//     scrollTop: $(id).offset().top - nav_height
+//   }, 500);
+//
+//   return false;
+// });
+
+// modal
+$(".modal-transparent").on('show.bs.modal', function () {
+  setTimeout( function() {
+    $(".modal-backdrop").addClass("modal-backdrop-transparent");
+  }, 0);
+});
+$(".modal-transparent").on('hidden.bs.modal', function () {
+  $(".modal-backdrop").addClass("modal-backdrop-transparent");
+});
+
+$(".modal-fullscreen").on('show.bs.modal', function () {
+  setTimeout( function() {
+    $(".modal-backdrop").addClass("modal-backdrop-fullscreen");
+  }, 0);
+});
+$(".modal-fullscreen").on('hidden.bs.modal', function () {
+  $(".modal-backdrop").addClass("modal-backdrop-fullscreen");
 });
